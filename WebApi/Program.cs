b873b773e1e1d7775946ca3c49f8cd6e575d322b
Lynn770 <lynn.locher@edu.gbssg.ch>
2025-05-21 14:03:31 +0200

    .

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -25,3 +25,51 @@ app.MapGet("/check", async (Microsoft.Extensions.Options.IOptions<DatabaseSettin
 });
 
 app.Run();
+
+ // Insert Movie
+ // Wenn das übergebene Objekt eingefügt werden konnte,
+ // wird es mit Statuscode 200 zurückgegeben.
+ // Bei Fehler wird Statuscode 409 Conflict zurückgegeben.
+ app.MapPost("/api/movies", (Movie movie) =>
+ {
+ throw new NotImplementedException();
+ });
+ // Get all Movies
+ // Gibt alle vorhandenen Movie-Objekte mit Statuscode 200 OK zurück.
+ app.MapGet("api/movies", () =>
+ {
+ throw new NotImplementedException();
+ });
+ // Get Movie by id
+ // Gibt das gewünschte Movie-Objekt mit Statuscode 200 OK zurück.
+ // Bei ungültiger id wird Statuscode 404 not found zurückgegeben.
+  app.MapGet("api/movies/{id}", (string id) =>
+   {
+        if(id == "1")
+            {
+            var myMovie = new Movie()
+            {
+                Id = "1",
+                Title = "Asterix und Obelix",
+            };
+            return Results.Ok(myMovie);
+        }
+        else
+        { 
+            return Results.NotFound();
+        }
+    });
+ // Update Movie
+ // Gibt das aktualisierte Movie-Objekt zurück.
+ // Bei ungültiger id wird Statuscode 404 not found zurückgegeben.
+ app.MapPut("/api/movies/{id}", (string id, Movie movie) =>
+ {
+ throw new NotImplementedException();
+ });
+ // Delete Movie
+ // Gibt bei erfolgreicher Löschung Statuscode 200 OK zurück.
+ // Bei ungültiger id wird Statuscode 404 not found zurückgegeben.
+ app.MapDelete("api/movies/{id}", (string id) =>
+ {
+ throw new NotImplementedException();
+ });
